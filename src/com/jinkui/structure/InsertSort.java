8f8631e9fa8da777586b336458fda0bff281f3e3
huangwei <blueburble@163.com>
2021-08-25 17:37:18 +0800

    选择排序总结

---
+++ src/com/jinkui/structure/InsertSort.java
@@ -0,0 +1,46 @@
+package com.jinkui.structure;
+
+import java.util.Arrays;
+
+public class InsertSort {
+
+    public static void main(String[] args){
+        int[] array = new int[]{4,6,1,3,3,2,1,9,80};
+        insertSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+
+    /**
+     * 插入排序
+     * 1. 时间复杂度: n²
+     * 2. 稳定、原地排序算法
+     * 3.空间复杂度 O(1)
+     * 4.平均时间复杂度n²
+     * 最好 n   最坏 n²
+     *
+     *
+     *
+     *
+     *
+     *
+     * @param list
+     */
+    static void insertSort(int[] list){
+        if(null == list || list.length <= 1){
+            return;
+        }
+        for(int i = 1; i < list.length; i++){
+            int value = list[i];
+            int j = i - 1;
+            for(; j >=0; j--){
+                if(list[j] > value){
+                    list[j + 1] = list[j];
+                }else {
+                    break;
+                }
+            }
+            list[j + 1] = value;
+        }
+    }
+}
---
+++ src/com/jinkui/structure/SelectSort.java
@@ -0,0 +1,46 @@
+package com.jinkui.structure;
+
+import java.util.Arrays;
+
+public class SelectSort {
+
+    public static void main(String[] args){
+            int[] array = new int[]{8,1,4,2,6,9,10};
+            selectSort(array);
+            System.out.println(Arrays.toString(array));
+
+    }
+
+    /***
+     * 选择排序
+     * 时间复杂度n²
+     * 空间复杂度O(1)
+     * 最好 最坏 n2
+     * 平均n2
+     * 非稳定排序
+     *
+     *
+     *
+     *
+     * @param list
+     */
+
+    static void selectSort(int[] list){
+        if(null == list || list.length <= 1){
+            return;
+        }
+        for(int i = 0; i < list.length; i++){
+            int min = i;
+            for(int j = i + 1; j < list.length; j ++){
+                if(list[j] <  list[min]){
+                    min = j;
+                }
+            }
+            if(i != min){
+                int temp = list[min];
+                list[min] = list[i];
+                list[i] = temp;
+            }
+        }
+    }
+}
